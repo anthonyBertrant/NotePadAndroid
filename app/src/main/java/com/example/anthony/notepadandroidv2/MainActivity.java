329b9d11e41b48c5be6cf145307923869cca0e20
anthony <anthony.bertrant@etu.univ-amu.fr>
2016-03-25 12:02:01 +0100

    creation de note et test dans l'activity

--- app/src/main/java/com/example/anthony/notepadandroidv2/MainActivity.java
+++ app/src/main/java/com/example/anthony/notepadandroidv2/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.anthony.notepadandroidv2;
 //Accueil de l'application
 
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +19,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*//creation de la BD et ouverture en ecriture
+        SQLiteDatabase db = NotesDBOpenHelper.getNoteBDOpenHelper(getBaseContext()).getWritableDatabase();
+
+        //creation de valeurs
+        //note 1
+        ContentValues values1 = new ContentValues();
+        values1.put(NotesDBOpenHelper.COLUMN_TITRE, "Note 1");
+        values1.put(NotesDBOpenHelper.COLUMN_CONTENU, "Voici une premiere note");
+        values1.put(NotesDBOpenHelper.COLUMN_DATE, "25/03/2016");
+
+        //note 2
+        ContentValues values2 = new ContentValues();
+        values2.put(NotesDBOpenHelper.COLUMN_TITRE, "Note 2");
+        values2.put(NotesDBOpenHelper.COLUMN_CONTENU, "Voici une deuxieme note");
+        values2.put(NotesDBOpenHelper.COLUMN_DATE, "25/03/2016");
+
+        //note 3
+        ContentValues values3 = new ContentValues();
+        values3.put(NotesDBOpenHelper.COLUMN_TITRE, "Note 3");
+        values3.put(NotesDBOpenHelper.COLUMN_CONTENU, "Voici une troisieme note");
+        values3.put(NotesDBOpenHelper.COLUMN_DATE, "25/03/2016");
+
+        //insert in DB
+        long id = db.insert(NotesDBOpenHelper.DATABASE_TABLE, null, values1);
+        id = db.insert(NotesDBOpenHelper.DATABASE_TABLE, null, values2);
+        id = db.insert(NotesDBOpenHelper.DATABASE_TABLE, null, values3);
+
+        String[] allColums = {NotesDBOpenHelper.COLUMN_TITRE, NotesDBOpenHelper.COLUMN_CONTENU, NotesDBOpenHelper.COLUMN_DATE};
+
+        //Explorer la BD
+        Cursor cursor = db.query(NotesDBOpenHelper.DATABASE_TABLE, allColums, null,null,null,null,null);
+        cursor.moveToFirst();
+        while(!cursor.isAfterLast()){
+            Button bouton = new Button(this);
+            bouton.setText(cursor.getString(0));
+        }*/
+        Date date = new Date();
+        Notes note1 = new Notes("note 1", "Ceci est une premeire note", date);
+        Notes note2 = new Notes("note 2", "Deuxieme note", date);
+
+
     }
 }
--- app/src/main/java/com/example/anthony/notepadandroidv2/Notes.java
+++ app/src/main/java/com/example/anthony/notepadandroidv2/Notes.java
@@ -5,6 +5,7 @@ import java.util.Date;
 /**
  * Created by anthony on 25/03/16.
  */
+
 public class Notes {
     private String titre;   //titre de la note
     private String contenu; //contenue de la note
@@ -15,5 +16,34 @@ public class Notes {
         this.titre = titre;
         this.contenu = contenu;
         this.date = date;
+    }
+
+    public String getTitre(){
+        return titre;
+    }
+
+    public String getContenu(){
+        return contenu;
+    }
+
+    public String getDate(){
+        return date.toString();
+    }
+
+    public void setTitre(String titre){
+        this.titre = titre;
+    }
+
+    public void setContenu(String contenu){
+        this.contenu = contenu;
+    }
+
+    @Override
+    public String toString() {
+        return "Notes{" +
+                "titre='" + titre + '\'' +
+                ", contenu='" + contenu + '\'' +
+                ", date=" + date +
+                '}';
     }
 }
